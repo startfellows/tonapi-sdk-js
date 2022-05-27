0cd50150dc3fbb2b6e7eaaf7103255481d2215bd
Vadim Malyutin <vmaliutin@Vadims-MacBook-Pro.local>
2022-05-27 19:21:06 +0400

    feat: 🎸 transpile to js es2015 with .d.ts

---
+++ dist/apis/index.d.ts
@@ -0,0 +1,6 @@
+export * from './AnnotationApi';
+export * from './JettonApi';
+export * from './NFTApi';
+export * from './RawBlockchainApi';
+export * from './TraceApi';
+export * from './WalletApi';
---
+++ dist/models/index.js
@@ -0,0 +1,43 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+/* tslint:disable */
+/* eslint-disable */
+__exportStar(require("./Account"), exports);
+__exportStar(require("./AccountAddress"), exports);
+__exportStar(require("./Annotation"), exports);
+__exportStar(require("./Annotations"), exports);
+__exportStar(require("./Block"), exports);
+__exportStar(require("./JettonBalance"), exports);
+__exportStar(require("./JettonInfo"), exports);
+__exportStar(require("./JettonsBalances"), exports);
+__exportStar(require("./Message"), exports);
+__exportStar(require("./ModelError"), exports);
+__exportStar(require("./NftCollection"), exports);
+__exportStar(require("./NftCollections"), exports);
+__exportStar(require("./NftForSale"), exports);
+__exportStar(require("./NftItem"), exports);
+__exportStar(require("./NftItems"), exports);
+__exportStar(require("./NftSalesResponse"), exports);
+__exportStar(require("./Trace"), exports);
+__exportStar(require("./TraceMsg"), exports);
+__exportStar(require("./TraceTX"), exports);
+__exportStar(require("./Traces"), exports);
+__exportStar(require("./Transaction"), exports);
+__exportStar(require("./Transactions"), exports);
+__exportStar(require("./TxAnnotation"), exports);
+__exportStar(require("./Wallet"), exports);
+__exportStar(require("./Wallets"), exports);
